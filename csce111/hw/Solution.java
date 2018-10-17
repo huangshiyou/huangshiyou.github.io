9f538174b7e6e46897fb7722d8f04ff37d5a2580
Shiyou Huang <huangsy@tamu.edu>
2018-10-16 23:39:27 -0500

    hw4

---
+++ csce111/hw/Solution.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+	
+    
+    //problem 2
+    public void pro2(int[] arr, int n) {
+        //you code starts here
+    }
+    
+    //problem 3
+    public boolean pro3(int[][] arr) {
+        //you code starts here
+    }
+    
+    //problem 4
+    public void pro4(int[] arr) {
+        //you code starts here
+    }
+
+}
